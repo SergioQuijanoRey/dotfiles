5dca06670561635eef7baa441392ebb77ba3dedf
Sergio Quijano <sergiquijano@gmail.com>
2019-03-29 10:28:51 +0100

    Refino los scripts de instalacion Ahora se pueden escribir comentarios en las listas de programas Reorganizo la distribucion de los paquetes

--- Installation/packages.sh
+++ Installation/packages.sh
@@ -1,33 +1,41 @@
 #!/bin/bash
 
-# Variables basicas
+# Author:
+# 		Sergio Quijano Rey
+# 		sergiquijano@gmail.com
+# Description:
+# 		Automatic package installer on arch-based systems
+# Version:
+# 		v1.0 - 29/03/2019: First documented version
+
+# Script parameters
 base_dir="/home/sergio/GitProjects/dotfiles/Installation/"
 pacman_list="$base_dir/package_list.txt"
 aur_list="$base_dir/aur_list.txt"
+aur_manager="yay"
 
-# Actualizo el sistema
-echo "Actualizando el sistema por completo..."
-sudo pacman -Syu
-echo "Sistema actualizado!"
+# System update
+echo "Uptading and upgrading the system"
+sudo pacman -Syyu
+echo "Update done!"
 echo ""
 
-# Instalo uno a uno los paquetes de pacman
-echo "Instalando los paquetes especificados en $pacman_list ..."
-for package in $(cat $pacman_list)
+# Pacman packages are installed
+echo "Installing pacman packages specified at $pacman_list ..."
+for package in $(cat $pacman_list | grep -o '^[^#]*')
 do
-	echo "Instalando $packages..."
+	echo "Installing $package..."
 	sudo pacman -S $package --noconfirm
 	echo ""
 done
 
-# Instalo uno a uno los paquetes de pacman
-echo "Instalando los paquetes especificados en $aur_list ..."
-for package in $(cat $aur_list)
+# AUR packages are installed
+echo "Installing AUR packages specified at $aur_list ..."
+for package in $(cat $aur_list | grep -o '^[^#]*')
 do
-	echo "Instalando $packages..."
-	yay  -S $package --noconfirm
+	echo "Installing $package..."
+	$aur_manager  -S $package --noconfirm
 	echo ""
 done
 
-echo "Paquetes instalados!"
-echo ""
+echo "Installation completed successfully!"
