b72b279e3cdcd6565dd794577604bc0808f1aa15
Sergio Quijano Rey <sergiquijano@gmail.c>
2019-04-16 19:14:15 +0200

    Sigo modificando algunos archivos para que esto funcione

--- installation_files/setup.sh
+++ installation_files/setup.sh
@@ -10,29 +10,20 @@
 
 # Script Parameters
 home="/home/sergio"
-dotfiles="$home/GitProjects/dotfiles"
-packages="$dotfiles/Installation/packages.sh"
-
-# Original files are removed
-rm -f $home/.bashrc
-rm -f $home/.bash_aliases
-rm -f $home/.profile
-rm -f $home/.vimrc
-rm -rf $home/.i3
-rm -rf $home/.config/ranger
-rm -rf $home/.config/nvim
-rm -rf $home/.cofig/alacritty
-rm -rf $home/bin
+base_dir="$home/GitProjects/dotfiles"
+dotfiles="$base_dir/config_files"
+package_installer="$base_dir/installation_files/packages.sh"
 
 # Symbolic links are set
 echo "Home: $home"
 echo "DOTFILES: $dotfiles"
 
-for file in .bashrc .bash_aliases .profile .Xresources .vimrc .i3 bin .config/ranger .config/nvim .config/alacritty 
+for file in $dotfiles/*
 do
-	origin="$dotfiles/$file"
-	dest="$home/$file"
-	ln -s $origin $dest
+	file2=${file%$base_dir}
+	echo "file: $file"
+	echo "file2: $file2"
+	#ln -s -f --target-directory=$home $file $file
 done
 
 # Custom crontab is added
