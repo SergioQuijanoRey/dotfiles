2b48b6238c22043555e89852bc64974b8d66d0f3
Sergio <sergiquijano@gmail.com>
2022-04-26 21:20:12 +0200

    feat: configured basic snippets using luasnip

--- config_files/.config/nvim/lua/myconf/completion.lua
+++ config_files/.config/nvim/lua/myconf/completion.lua
@@ -7,6 +7,13 @@
 -- Setup nvim-cmp.
 local cmp = require'cmp'
 
+-- Integration with luasnip
+local luasnip = require("luasnip")
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 -- Min word length to trigger autcompletion
 -- I use same min for all completion contexts
 local min_keyword_length = 3
@@ -36,16 +43,43 @@ cmp.setup({
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+
+        -- Luasnip keybindings
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+
+
     }),
 
     sources = cmp.config.sources({
         { name = 'nvim_lsp', keyword_length = min_keyword_length },
         -- { name = 'vsnip' }, -- For vsnip users.
-        { name = 'luasnip', keyword_length = min_keyword_length  }, -- For luasnip users.
+        { name = 'luasnip', keyword_length = min_keyword_length }, -- For luasnip users.
         -- { name = 'ultisnips' }, -- For ultisnips users.
         -- { name = 'snippy' }, -- For snippy users.
     }, {
-        { name = 'buffer', keyword_length = min_keyword_length, option = { keyword_pattern = [[\k\+]]}  },
+        { name = 'buffer', keyword_length = min_keyword_length, option = {
+            keyword_pattern = [[\k\+]]
+        }  },
     }
 
 
---
+++ config_files/.config/nvim/lua/myconf/snippets.lua
@@ -0,0 +1,68 @@
+-- Module where I define some snippets using luasnip
+
+-- First configure some luasnip behavior
+local ls = require("luasnip")
+local types = require "luasnip.util.types"
+
+ls.config.set_config {
+  -- This tells LuaSnip to remember to keep around the last snippet.
+  -- You can jump back into it even if you move outside of the selection
+  history = true,
+
+  -- This one is cool cause if you have dynamic snippets, it updates as you type!
+  updateevents = "TextChanged,TextChangedI",
+
+  -- Autosnippets:
+  enable_autosnippets = true,
+
+  -- Good highlights!!
+  ext_opts = {
+    [types.choiceNode] = {
+      active = {
+        virt_text = { { " <- Current Choice", "NonTest" } },
+      },
+    },
+  },
+}
+
+-- Shorthands for defining snippets
+local s = ls.snippet
+local sn = ls.snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local r = ls.restore_node
+local l = require("luasnip.extras").lambda
+local rep = require("luasnip.extras").rep
+local p = require("luasnip.extras").partial
+local m = require("luasnip.extras").match
+local n = require("luasnip.extras").nonempty
+local dl = require("luasnip.extras").dynamic_lambda
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local types = require("luasnip.util.types")
+local conds = require("luasnip.extras.expand_conditions")
+
+ls.add_snippets("all", {
+    -- trigger is `fn`, second argument to snippet-constructor are the nodes to insert into the buffer on expansion.
+    s("expand", fmt(
+        "-- Sergio says: {}\n-- Category is {}\n--Then {} says {}",
+        {i(1, "default comment"), i(2, "category"), rep(1), rep(2)}
+    )),
+}, {
+    key = "all",
+})
+
+
+ls.add_snippets("lua", {
+
+    -- Easily require a function
+    s("require", fmt(
+        "local {} = require(\"{}\")",
+        {i(1, "plug_name"), rep(1)}
+    )),
+}, {
+    key = "lua",
+})
