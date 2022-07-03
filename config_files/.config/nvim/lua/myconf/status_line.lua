0e4cac907231cb2e03f74d482d8c3cd4855ec475
Sergio <sergiquijano@gmail.com>
2022-07-03 16:40:50 +0200

    feat: added lsp servers in nvim status line

--- config_files/.config/nvim/lua/myconf/status_line.lua
+++ config_files/.config/nvim/lua/myconf/status_line.lua
@@ -1,6 +1,25 @@
 -- Config for the status line that i am using
 
-require'lualine'.setup {
+-- Inspired by https://github.com/nvim-lualine/lualine.nvim/blob/master/examples/evil_lualine.lua
+-- Changed a little bit the function and added some symbols. Also not using ins_left function
+local function lsp_server_names()
+    local msg = 'No Active Lsp'
+    local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+    local clients = vim.lsp.get_active_clients()
+    if next(clients) == nil then
+        return " " .. msg
+    end
+    for _, client in ipairs(clients) do
+        local filetypes = client.config.filetypes
+        if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+            return " " .. client.name
+        end
+    end
+    msg = " " .. msg
+    return msg
+end
+
+local config = {
   options = {
     icons_enabled = true,
     theme = 'catppuccin',
@@ -14,7 +33,11 @@ require'lualine'.setup {
     lualine_b = {'branch', 'diff',
                   {'diagnostics', sources={'nvim_diagnostic', 'coc'}}},
     lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_x = { lsp_server_names,
+
+
+        'encoding', 'fileformat', 'filetype'
+    },
     lualine_y = {'progress'},
     lualine_z = {'location'}
   },
@@ -29,3 +52,7 @@ require'lualine'.setup {
   tabline = {},
   extensions = {}
 }
+
+
+
+require'lualine'.setup(config)
