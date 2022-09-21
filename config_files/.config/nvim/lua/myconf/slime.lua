523fbb43c433f25f045d5636e4042142c2ab1e7a
Sergio Quijano <sergiquijano@gmail.com>
2022-09-21 13:40:38 +0200

    fix [nvim]: slime was creating '$HOME' variables everywhere

--- config_files/.config/nvim/lua/myconf/slime.lua
+++ config_files/.config/nvim/lua/myconf/slime.lua
@@ -8,7 +8,8 @@ vim.g.slime_target = "tmux"
 
 -- The file used for it has a screen name
 -- So set a tmux name for more clarity
-vim.g.slime_paste_file = "$HOME/.tmux_slime"
+HOME = os.getenv("HOME")
+vim.g.slime_paste_file = HOME .. "/.tmux_slime"
 
 -- Disable default mappings and set custom ones
 vim.g.slime_no_mappings = 1
