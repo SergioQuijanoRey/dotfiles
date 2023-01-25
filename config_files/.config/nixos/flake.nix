ccc5a2c60d0006a126a6b5316589718c6fe981e4
Sergio Quijano <sergiquijano@gmail.com>
2023-01-25 14:17:31 +0100

    fix: deleted section in flake.nix that was not used

--- config_files/.config/nixos/flake.nix
+++ config_files/.config/nixos/flake.nix
@@ -4,30 +4,27 @@
     # Dependencies of the flake
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
         home-manager = {
             url = "github:nix-community/home-manager";
 
             # So we can use nixpkgs instead of home manager packages
             inputs.nixpkgs.follows = "nixpkgs";
         };
-
-
     };
 
     outputs = { self, nixpkgs, home-manager }:
-
-        let
+    let
         # Architecture of the system
         system = "x86_64-linux";
         pkgs = import nixpkgs {
             inherit system;
             config.allowUnfree = true;
         };
-
         lib = nixpkgs.lib;
     in {
         nixosConfigurations = {
-            sergio = lib.nixosSystem {
+            asus-laptop = lib.nixosSystem {
                 inherit system;
                 modules = [
                     # Load the base NixOS configuration
@@ -44,19 +41,6 @@
                         };
                     }
                 ];
-            };
-        };
-
-
-        hmConfig = {
-            sergio = home-manager.lib.homeManagerConfiguration {
-                    inherit system pkgs;
-                    username = "sergio";
-                    homeDirectory = "/home/sergio";
-                    stateVersion  = "22.05";
-                    configuration = {
-                            imports = [ ./home.nix ];
-                    };
             };
         };
     };
