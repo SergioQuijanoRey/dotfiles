359f4acc407fa4f735a5949305a9d698dab40857
Sergio Quijano <sergiquijano@gmail.com>
2023-04-19 19:12:47 +0200

    chore: updated the version of `timer` package that I custom build

--- config_files/.config/nixos/custom_packages/timer.nix
+++ config_files/.config/nixos/custom_packages/timer.nix
@@ -5,14 +5,15 @@ buildGoModule rec {
   pname = "timer";
   version = "1.2.0";
 
+  # Use `github.com/nix-community/nurl` to get this data
   src = fetchFromGitHub {
     owner = "caarlos0";
     repo = "timer";
-    rev = "v${version}";
-    sha256 = "sha256-/HGr1TT+oYEub4N0uf0Wvd18GhJBtruPTZr9DHhG4rU=";
+    rev = "50561bc33b32d1a07cddf8264e3cbb134f425463";
+    sha256 = "sha256-9p/L3Hj3VqlNiyY3lfUAhCjwTl1iSTegWxaVEGB4qHM=";
   };
 
-  vendorHash = "sha256-EXb/Nz3dTTUUHZ9f55cDnQyrD8biRbgcovQEYi4LH74=";
+  vendorHash = "sha256-j7Xik0te6GdjfhXHT7DRf+MwM+aKjfgTGvroxnlD3MM=";
 
   meta = with lib; {
     description = "A `sleep` with progress";
