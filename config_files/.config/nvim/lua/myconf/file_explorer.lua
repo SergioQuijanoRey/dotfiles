9d8fe1fcd1d5bd839d3d47612a0706a4695169bf
Sergio Quijano <sergiquijano@gmail.com>
2023-05-24 18:20:00 +0200

    feat: neovim file explorer now opens in float mode

--- config_files/.config/nvim/lua/myconf/file_explorer.lua
+++ config_files/.config/nvim/lua/myconf/file_explorer.lua
@@ -3,4 +3,19 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- empty setup using defaults
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+    view = {
+        float = {
+            enable = true,
+            quit_on_focus_loss = true,
+            open_win_config = {
+                relative = "editor",
+                border = "rounded",
+                width = 30,
+                height = 30,
+                row = 1,
+                col = 1,
+            },
+        },
+    },
+})
