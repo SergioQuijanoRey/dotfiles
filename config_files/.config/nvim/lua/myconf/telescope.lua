934e9c03fcae6d4a36fe9fc1c1bc35bdd56bf699
Sergio Quijano <sergiquijano@gmail.com>
2023-07-07 13:40:00 +0200

    feat: nvim telescope now searches in hidden files (dotfiles)

--- config_files/.config/nvim/lua/myconf/telescope.lua
+++ config_files/.config/nvim/lua/myconf/telescope.lua
@@ -1,6 +1,27 @@
- require('telescope').setup{
-  defaults = {
-    -- We are using the default sorter, that now works better than the rest
-    file_sorter = require'telescope.sorters'.get_fuzzy_file,
-  }
+local telescope = require("telescope")
+local telescopeConfig = require("telescope.config")
+
+-- Construct the arguments to make live grep search also in hidden files
+local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) } -- Copy default arguments
+table.insert(vimgrep_arguments, "--hidden")                                    -- Search in hidden files
+table.insert(vimgrep_arguments, "--glob")
+table.insert(vimgrep_arguments, "!**/.git/*")                                  -- But don't search in .git folder
+
+telescope.setup{
+    defaults = {
+        -- We are using the default sorter, that now works better than the rest
+        file_sorter = require'telescope.sorters'.get_fuzzy_file,
+
+        -- Live grep also searches in hidden files (dotfiles in Unix)
+        vimgrep_arguments = vimgrep_arguments,
+    },
+
+
+    pickers = {
+        -- Find files also in hidden files (dotfiles in Unix)
+        find_files = {
+            -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+            find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+        },
+    },
 }
