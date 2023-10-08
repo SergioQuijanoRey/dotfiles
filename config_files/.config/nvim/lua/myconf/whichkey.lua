794563b6c544c0b1609057897286d7f832b27cc5
Sergio Quijano <sergiquijano@gmail.com>
2023-10-08 20:12:32 +0200

    feat: disabled some whichkey help menus

--- config_files/.config/nvim/lua/myconf/whichkey.lua
+++ config_files/.config/nvim/lua/myconf/whichkey.lua
@@ -1,5 +1,19 @@
 require("which-key").setup {
-  -- your configuration comes here
-  -- or leave it empty to use the default settings
-  -- refer to the configuration section below
+
+    plugins = {
+        presets = {
+            -- This presets are too much for me
+            -- I know pretty well this vim default bindings
+            operators = false,
+            motions = false,
+
+            -- I also know this bindings but are more useful to show
+            -- Also I don't use them as much as the disabled ones
+            text_objects = true,
+            windows = true,
+            nav = true,
+            z = true,
+            g = true,
+        }
+    }
 }
