4cfcc15c1a41de27fcdf1c12bfc5f80667c424c4
Sergio Quijano Rey <sergiquijano@gmail.com>
2024-01-03 16:52:01 +0100

    feat: better zen configuration

--- config_files/.config/nvim/lua/myconf/zen.lua
+++ config_files/.config/nvim/lua/myconf/zen.lua
@@ -1,35 +1,55 @@
 local setmap = require("myconf.aux").setmap
+local setmap_group_name = require("myconf.aux").setmap_group_name
 local exec = require("myconf/aux").exec
 
 
+-- We are going to have two zen mode configurations
+-- So the following table is going to be the default config
+local template_config = {
+    window = {
+        backdrop = 1.0,     -- Keep the same color in the background
+        width = 0.8,        -- 80% of the width
+        options = {
+            number = false, -- Disable numbers in zen mode
+            foldcolumn = "0",
+        },
+    },
+
+    on_open = function(window)
+        -- Wrap lines when entering zen mode.
+        exec(':set wrap')
+
+        -- Make wrapping preservecode identation
+        exec(':set breakindent')
+
+        -- Don't add a newline in the middle of a word when wrapping
+        exec(':set linebreak')
+
+        -- Disable 80 char column line
+        vim.opt.colorcolumn = nil
+    end,
+
+    -- Run changes when toggling off zen mode
+    --
+    -- Note that visual modifications done in `on_open` are lost when exiting
+    -- zen mode, so no need to manually undoing changes
+    on_close = function()
+    end
+}
+
 -- Function that is going to run when triggering the keybinding
 -- In this function we configure how the zen mode looks
-local custom_zen = function()
-    require("zen-mode").toggle({
-        window = {
-            backdrop = 1.0, -- Keep the same color in the background
-            width = 0.95,
-            options = {
-                number = false, -- Disable numbers in zen mode
-                foldcolumn = "0",
-            },
-        },
+local custom_zen = function(width_percentage)
+    -- Copy the default config and change the width percentage
+    local new_config = template_config
+    new_config.window.width = width_percentage
 
-        -- Wrap lines when entering zen mode
-        on_open = function(window)
-            exec(':set wrap linebreak')
-            vim.opt.colorcolumn = nil
-        end,
-
-        -- Run changes when toggling off zen mode
-        --
-        -- Note that visual modifications done in `on_open` are lost when exiting
-        -- zen mode, so no need to manually undoing changes
-        on_close = function()
-        end
-    })
+    -- Apply that configuration
+    require("zen-mode").toggle(new_config)
 end
 
 
 -- Keybinding that uses our custom configuration
-setmap("n", "<leader>f", custom_zen, {}, "Toggle on and off zen mode")
+setmap_group_name("<leader>f", "Zen mode")
+setmap("n", "<leader>ff", function() custom_zen(0.8) end, {}, "Toggle on and off zen mode, small mode")
+setmap("n", "<leader>fF", function() custom_zen(0.95) end, {}, "Toggle on and off zen mode, big mode")
