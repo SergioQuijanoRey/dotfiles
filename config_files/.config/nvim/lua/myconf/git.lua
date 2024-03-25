32199cc6c716b113b5d1da3c353b49021c7218d8
Sergio Quijano Rey <sergiquijano@gmail.com>
2024-03-25 13:30:00 +0100

    feat: improved a bit the neogit configuration

--- config_files/.config/nvim/lua/myconf/git.lua
+++ config_files/.config/nvim/lua/myconf/git.lua
@@ -16,14 +16,22 @@ require('gitsigns').setup {
 -- Neogit
 -- =============================================================================
 require('neogit').setup({
-    graph_style = "ascii",
+    -- This makes some parts look better
+    graph_style = "unicode",
+
+    -- Open neogit "popups" in split mode by default, instead of tab mode
+    -- Vertical or horizontal split are chosen automatically
     kind = "auto",
+
+    -- I want to open the commit editor in normal mode instead of insert mode
+    disable_insert_on_commit = true,
+
     integrations = {
         telescope = "true",
         diffview = "true",
     },
 
-    -- I want to be able to use "w" and "b" for navigating
+    -- I want to be able to use "w" and "b" for navigating    -- I want to be able to use "w" and "b" for navigating    -- I want to be able to use "w" and "b" for navigating    -- I want to be able to use "w" and "b" for navigating
     -- So change these sortcuts to "Shift-w" and "Shift-b"
     mappings = {
         popup = {
@@ -34,6 +42,7 @@ require('neogit').setup({
             -- And set them with new things
             ["W"] = "WorktreePopup",
             ["B"] = "BranchPopup",
+
         },
     },
 
