5f007783737c6493d0b347b574e78d58a1615c5d
Sergio Quijano Rey <sergiquijano@gmail.com>
2024-04-12 13:53:25 +0200

    feat: better snippet for obsidian header. Now with date, fixed issues

--- config_files/.config/nvim/lua/plugins/snippets.lua
+++ config_files/.config/nvim/lua/plugins/snippets.lua
@@ -51,6 +51,16 @@ return {
         local fmt = require("luasnip.extras.fmt").fmt
         local fmta = require("luasnip.extras.fmt").fmta
         local types = require("luasnip.util.types")
+
+        -- Returns a snippet_node wrapped around an insertNode whose initial
+        -- text value is set to the current date in the desired format.
+        local date_input = function(args, snip, old_state, fmt)
+            local fmt = fmt or "%Y-%m-%d"
+            return sn(nil, i(1, os.date(fmt)))
+        end
+
+
+
         local conds = require("luasnip.extras.expand_conditions")
 
         ls.add_snippets("lua", {
@@ -73,16 +83,21 @@ return {
             )),
 
             s("obsidianheader", fmt(
-                [[
-	---
-	title: {}
-	type: {}
-	tags: {}
-    created_at: {}
-    last_modified: {}
-	---
-	]],
-                { i(1, ""), i(2, "note"), i(3, ""), i(4, ""), i(5, "") }
+[[
+---
+title: {}
+type: {}
+tags: {}
+created_at: {}
+last_modified: {}
+---
+]],
+                {
+                    i(1, ""),
+                    i(2, "note"), i(3, ""),
+                    d(4, date_input, {}, { user_args = { "%d/%m/%Y" } }),
+                    d(5, date_input, {}, { user_args = { "%d/%m/%Y" } }),
+                }
             )),
 
             s("task", fmt(
