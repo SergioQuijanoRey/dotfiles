033a3301c20d429a08b994d9f92c028b268d2c24
Sergio Quijano Rey <sergiquijano@gmail.com>
2024-11-06 12:41:03 +0100

    feat: better nvim whichkey visuals

--- config_files/.config/nvim/lua/plugins/keybindings.lua
+++ config_files/.config/nvim/lua/plugins/keybindings.lua
@@ -1,33 +1,36 @@
 return {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    opts = {
-        plugins = {
-            presets = {
-                -- This presets are too much for me
-                -- I know pretty well this vim default bindings
-                operators = false,
-                motions = false,
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	opts = {
+		plugins = {
+			presets = {
+				-- This presets are too much for me
+				-- I know pretty well this vim default bindings
+				operators = false,
+				motions = false,
 
-                -- I also know this bindings but are more useful to show
-                -- Also I don't use them as much as the disabled ones
-                text_objects = true,
-                windows = true,
-                nav = true,
-                z = true,
-                g = true,
-            }
-        },
-        -- Helix windows
-        preset = "helix",
-        win = {
-            width = { min = 10, max = 100 },
-            height = { min = 50, max = 100 },
-        },
-    },
-    init = function()
-        vim.o.timeout = true
-        vim.o.timeoutlen = 300
-        local root = vim.fn.stdpath("data") .. "/lazy"
-    end,
+				-- I also know this bindings but are more useful to show
+				-- Also I don't use them as much as the disabled ones
+				text_objects = true,
+				windows = true,
+				nav = true,
+				z = true,
+				g = true,
+			},
+		},
+		-- Helix windows
+		preset = "helix",
+
+		-- Put the popup window in the left and make it bigger
+		win = {
+			height = { min = 20, max = math.huge },
+			row = math.huge,
+			col = 0,
+		},
+	},
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 300
+		local root = vim.fn.stdpath("data") .. "/lazy"
+	end,
 }
