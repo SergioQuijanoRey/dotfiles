33ee50f7117b263a1e7dd68f09b676abb1da00eb
Sergio Quijano <sergiquijano@gmail.com>
2024-11-13 20:39:26 +0100

    feat: changed the partitions of the workstation

--- config_files/.config/nixos/workstation/hardware-configuration.nix
+++ config_files/.config/nixos/workstation/hardware-configuration.nix
@@ -8,18 +8,18 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "thunderbolt" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "thunderbolt" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/6d56fc40-f6bd-4b6f-8b0c-dc3209e99c8f";
+    { device = "/dev/disk/by-uuid/072319b9-49da-4cf4-ab66-6544c98a10fe";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/07AD-1796";
+    { device = "/dev/disk/by-uuid/6EA1-9AAB";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
