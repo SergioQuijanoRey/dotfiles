bc1991a37ac2a7086ec76c99a4fd3890214fa3f7
Sergio Quijano Rey <sergiquijano@gmail.com>
2024-12-18 14:32:09 +0100

    feat: using blink.cmp instead of nvim.cmp as completion engine

--- config_files/.config/nvim/lua/plugins/snippets.lua
+++ config_files/.config/nvim/lua/plugins/snippets.lua
@@ -1,37 +1,33 @@
 return {
-    'L3MON4D3/LuaSnip',
+	"L3MON4D3/LuaSnip",
 
-    dependencies = {
-        -- For having autocompletion of the snippets trigger keywords
-        'saadparwaiz1/cmp_luasnip'
-    },
-    config = function()
-        -- First configure some luasnip behavior
-        local ls = require("luasnip")
-        local types = require "luasnip.util.types"
+	config = function()
+		-- First configure some luasnip behavior
+		local ls = require("luasnip")
+		local types = require("luasnip.util.types")
 
-        ls.config.set_config {
-            -- This tells LuaSnip to remember to keep around the last snippet.
-            -- You can jump back into it even if you move outside of the selection
-            history = true,
+		ls.config.set_config({
+			-- This tells LuaSnip to remember to keep around the last snippet.
+			-- You can jump back into it even if you move outside of the selection
+			history = true,
 
-            -- This one is cool cause if you have dynamic snippets, it updates as you type!
-            updateevents = "TextChanged,TextChangedI",
+			-- This one is cool cause if you have dynamic snippets, it updates as you type!
+			updateevents = "TextChanged,TextChangedI",
 
-            -- Autosnippets:
-            enable_autosnippets = true,
+			-- Autosnippets:
+			enable_autosnippets = true,
 
-            -- Good highlights!!
-            ext_opts = {
-                [types.choiceNode] = {
-                    active = {
-                        virt_text = { { " <- Current Choice", "NonTest" } },
-                    },
-                },
-            },
-        }
+			-- Good highlights!!
+			ext_opts = {
+				[types.choiceNode] = {
+					active = {
+						virt_text = { { " <- Current Choice", "NonTest" } },
+					},
+				},
+			},
+		})
 
-        -- Finally, require all the snippet files in the `snippets` folder
-        require("snippets")
-    end
+		-- Finally, require all the snippet files in the `snippets` folder
+		require("snippets")
+	end,
 }
--- config_files/.config/nvim/lua/snippets/typst.lua
+++ config_files/.config/nvim/lua/snippets/typst.lua
@@ -1,6 +1,6 @@
 -- First configure some luasnip behavior
 local ls = require("luasnip")
-local types = require "luasnip.util.types"
+local types = require("luasnip.util.types")
 
 -- Shorthands for defining snippets
 local s = ls.snippet
@@ -14,29 +14,18 @@ local FILETYPE = "typst"
 
 -- Make adding new snippets easier, instead of having a gigantic table
 local add_snippet = function(snippets, snippet_name, snippet_format)
-    local new_snippet = s(
-        snippet_name,
-        snippet_format
-    )
-    return table.insert(snippets, new_snippet)
+	local new_snippet = s(snippet_name, snippet_format)
+	return table.insert(snippets, new_snippet)
 end
 
 local snippets = {}
 
 -- Easily create footnotes with marks
-add_snippet(snippets, "footnotemark", fmt(
-    "#footnote([#last_footnote])",
-    {},
-    { delimiters = "<>" }
-))
+add_snippet(snippets, "footnotemark", fmt("#footnote([#last_footnote])", {}, { delimiters = "<>" }))
 
-add_snippet(snippets, "footnotetext", fmt(
-    "#let last_footnote = [<>]",
-    { i(1) },
-    { delimiters = "<>" }
-))
+add_snippet(snippets, "footnotetext", fmt("#let last_footnote = [<>]", { i(1) }, { delimiters = "<>" }))
 
 -- Finally, inform lua snip that we want to set this snippets
 ls.add_snippets(FILETYPE, snippets, {
-    key = FILETYPE,
+	key = FILETYPE,
 })
