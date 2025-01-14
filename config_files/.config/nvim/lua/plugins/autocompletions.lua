f2b9a0d5b792e39d57ed2188dc3fd93205b6bd21
Sergio Quijano Rey <sergio@NTTD-8B97J64.>
2025-01-14 12:31:09 +0100

    fix: problems in neovim configs

--- config_files/.config/nvim/lua/plugins/autocompletions.lua
+++ config_files/.config/nvim/lua/plugins/autocompletions.lua
@@ -1,48 +1,34 @@
 return {
-    {
-        "saghen/blink.cmp",
-        dependencies = {
-            -- We are going to autocomplete the snippets that LuaSnip manages
-            "L3MON4D3/LuaSnip",
-        },
-        -- use a release tag to download pre-built binaries
-        version = "v0.*",
-        opts = {
-            -- 'default' for mappings similar to built-in completion
-            -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-            -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-            -- see the "default configuration" section below for full documentation on how to define
-            -- your own keymap.
-            keymap = { preset = "enter" },
+	{
+		"saghen/blink.cmp",
+		dependencies = {
+			-- We are going to autocomplete the snippets that LuaSnip manages
+			"L3MON4D3/LuaSnip",
+		},
+		-- use a release tag to download pre-built binaries
+		version = "v0.*",
+		opts = {
+			-- 'default' for mappings similar to built-in completion
+			-- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+			-- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+			-- see the "default configuration" section below for full documentation on how to define
+			-- your own keymap.
+			keymap = { preset = "enter" },
 
-            appearance = {
-                use_nvim_cmp_as_default = true,
-                nerd_font_variant = "mono",
-            },
+			appearance = {
+				use_nvim_cmp_as_default = true,
+				nerd_font_variant = "mono",
+			},
 
-            snippets = {
-                expand = function(snippet)
-                    require("luasnip").lsp_expand(snippet)
-                end,
-                active = function(filter)
-                    if filter and filter.direction then
-                        return require("luasnip").jumpable(filter.direction)
-                    end
-                    return require("luasnip").in_snippet()
-                end,
-                jump = function(direction)
-                    require("luasnip").jump(direction)
-                end,
-            },
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer" },
+				-- optionally disable cmdline completions
+				-- cmdline = {},
+				min_keyword_length = 3,
+			},
 
-            sources = {
-                default = { "lsp", "path", "snippets", "buffer", "luasnip" },
-                -- optionally disable cmdline completions
-                -- cmdline = {},
-                min_keyword_length = 3,
-            },
-
-            signature = { enabled = true },
-        },
-    },
+			snippets = { preset = "luasnip" },
+			signature = { enabled = true },
+		},
+	},
 }
--- config_files/.config/nvim/lua/plugins/autoformat.lua
+++ config_files/.config/nvim/lua/plugins/autoformat.lua
@@ -1,23 +1,23 @@
 return {
-    "stevearc/conform.nvim",
-    event = { "BufWritePre" },
-    cmd = { "ConformInfo" },
-    opts = {
-        formatters_by_ft = {
-            lua = { "stylua" },
-            python = { "ruff_format", "isort" },
-            sql = { "sleek" },
-            typst = { "typstfmt" },
-            nix = { "alejandra" }
-        },
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "ruff_format", "isort" },
+			sql = { "sleek" },
+			typst = { "typstfmt" },
+			nix = { "alejandra" },
+		},
 
-        -- To utilize our user-defined functions to enable/disable autoformating
-        format_on_save = function(bufnr)
-            -- Disable with a global or buffer-local variable
-            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-                return
-            end
-            return { timeout_ms = 500, lsp_fallback = true }
-        end,
-    },
+		-- To utilize our user-defined functions to enable/disable autoformating
+		format_on_save = function(bufnr)
+			-- Disable with a global or buffer-local variable
+			if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+				return
+			end
+			return { timeout_ms = 500, lsp_fallback = true }
+		end,
+	},
 }
