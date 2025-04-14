a25eddef9ab4cfeebbd4afaa7aceeb4a94faf8ee
Sergio Quijano Rey <sergiquijano@gmail.com>
2025-04-14 14:16:55 +0200

    feat: clipboard support when working inside WSL

---
+++ config_files/.config/nvim/lua/myconf/wsl.lua
@@ -0,0 +1,15 @@
+local IS_WSL = true
+if IS_WSL then
+	vim.g.clipboard = {
+		name = "WslClipboard",
+		copy = {
+			["+"] = "clip.exe",
+			["*"] = "clip.exe",
+		},
+		paste = {
+			["+"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+			["*"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+		},
+		cache_enabled = 0,
+	}
+end
