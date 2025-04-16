ea5c2a2c99857fb844a02ad7335c910f25d71dd1
Sergio Quijano Rey <sergiquijano@gmail.com>
2025-04-16 17:58:23 +0200

    style: autoformat nvim config files

--- config_files/.config/nvim/lua/plugins/sql.lua
+++ config_files/.config/nvim/lua/plugins/sql.lua
@@ -1,56 +1,68 @@
 return {
-    {
-        'kristijanhusak/vim-dadbod-ui',
-        dependencies = {
-            {
-                'tpope/vim-dadbod',
-                lazy = true
-            },
-            {
-                'kristijanhusak/vim-dadbod-completion',
-                ft = { 'sql', 'mysql', 'plsql' },
-                lazy = true
-            },
-        },
-        cmd = {
-            'DBUI',
-            'DBUIToggle',
-            'DBUIAddConnection',
-            'DBUIFindBuffer',
-        },
-        init = function()
-            vim.g.db_ui_use_nerd_fonts = 1
-            vim.g.dbs = {
-                { name = "dev", url = "sqlite:///~/pruebas.sqlite" },
-            }
-            vim.g.db_ui_save_location = vim.fn.getcwd() .. "/sql_queries"
-            vim.g.db_ui_execute_on_save = 0
-            vim.g.db_ui_show_database_icon = 1
-        end
-    },
+	{
+		"kristijanhusak/vim-dadbod-ui",
+		dependencies = {
+			{
+				"tpope/vim-dadbod",
+				lazy = true,
+			},
+			{
+				"kristijanhusak/vim-dadbod-completion",
+				ft = { "sql", "mysql", "plsql" },
+				lazy = true,
+			},
+		},
+		cmd = {
+			"DBUI",
+			"DBUIToggle",
+			"DBUIAddConnection",
+			"DBUIFindBuffer",
+		},
+		init = function()
+			vim.g.db_ui_use_nerd_fonts = 1
+			vim.g.dbs = {
+				{ name = "dev", url = "sqlite:///~/pruebas.sqlite" },
+				{
+					name = "Escuela de Ventas",
+					url = "duckdb:////mnt/c/Users/squijanr/OneDrive - NTT DATA EMEAL/Projects/03 - EDV Sharepoint Privado/6. Data Warehouse/warehouse.duckdb",
+				},
+				{
+					name = "Generador de preguntas",
+					url = "duckdb:////mnt/c/Users/squijanr/OneDrive - NTT DATA EMEAL/Projects/03 - EDV Sharepoint Privado/4. Limpiezas & Entregables/10. OranGEN Learning/saved_questions.duckdb",
+				},
+			}
+			vim.g.db_ui_save_location = vim.fn.getcwd() .. "/sql_queries"
+			vim.g.db_ui_execute_on_save = 0
+			vim.g.db_ui_show_database_icon = 1
+		end,
+	},
 
-    {
-        "kndndrj/nvim-dbee",
-        dependencies = {
-            "MunifTanjim/nui.nvim",
-        },
-        build = function()
-            -- Install tries to automatically detect the install method.
-            -- if it fails, try calling it with one of these parameters:
-            --    "curl", "wget", "bitsadmin", "go"
-            require("dbee").install()
-        end,
-        config = function()
-            require("dbee").setup({
-                -- sources = {
-                --     require("dbee.sources").MemorySource:new(
-                --         {
-                --             name = "dev",
-                --             type = "sqlite",
-                --             url = "/home/sergio/pruebas.sqlite",
-                --         })
-                -- }
-            })
-        end,
-    }
+	{
+		"kndndrj/nvim-dbee",
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+		},
+		build = function()
+			-- Install tries to automatically detect the install method.
+			-- if it fails, try calling it with one of these parameters:
+			--    "curl", "wget", "bitsadmin", "go"
+			require("dbee").install()
+		end,
+		config = function()
+			require("dbee").setup({
+				sources = {
+					require("dbee.sources").MemorySource:new({
+						name = "edv",
+						type = "duck",
+						url = "/mnt/c/Users/squijanr/OneDrive - NTT DATA EMEAL/Projects/03 - EDV Sharepoint Privado/6. Data Warehouse/warehouse.duckdb",
+					}),
+					require("dbee.sources").MemorySource:new({
+						name = "pruebas",
+						type = "sqlite",
+						url = "~/pruebas.",
+					}),
+				},
+			})
+		end,
+	},
 }
--- config_files/.config/nvim/lua/plugins/visuals.lua
+++ config_files/.config/nvim/lua/plugins/visuals.lua
@@ -1,36 +1,41 @@
 return {
 
-    -- Identation guidelines
-    {
-        'lukas-reineke/indent-blankline.nvim',
-        main = "ibl",
-        config = function()
-            require("ibl").setup({
-                indent = {
-                    char = "|",
-                },
+	-- Identation guidelines
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		config = function()
+			require("ibl").setup({
+				indent = {
+					char = "|",
+				},
 
-                -- I don't want to hightlight the scope I am currently at
-                scope = { enabled = false },
-            })
-        end
-    },
+				-- I don't want to hightlight the scope I am currently at
+				scope = { enabled = false },
+			})
+		end,
+	},
 
-    -- Good pastel palette
-    {
-        'catppuccin/nvim',
-        name = "catppuccin",
-        priority = 1000,
-        opts = {
-            flavour = "frappe",
-            integrations = {
-                lsp_trouble = true,
-                which_key = true
-            }
-        },
-        config = function(_, opts)
-            require("catppuccin").setup(opts)
-            vim.api.nvim_command "colorscheme catppuccin-frappe"
-        end
-    },
+	-- Good pastel palette
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		opts = {
+			flavour = "frappe",
+			integrations = {
+				lsp_trouble = true,
+				which_key = true,
+			},
+		},
+		config = function(_, opts)
+			require("catppuccin").setup(opts)
+			vim.api.nvim_command("colorscheme catppuccin-frappe")
+		end,
+	},
+
+	-- Nice colorizer for hex and rgb codes
+	{
+		"norcalli/nvim-colorizer.lua",
+	},
 }
