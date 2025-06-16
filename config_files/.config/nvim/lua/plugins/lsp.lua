5efebcafbbd984eff8c9b6b998d9971c5e3a42aa
Sergio Quijano Rey <sergiquijano@gmail.com>
2025-06-16 13:39:56 +0200

    feat: new lsp config for neovim (still using `nvimlsp-config`)

--- config_files/.config/nvim/lua/plugins/lsp.lua
+++ config_files/.config/nvim/lua/plugins/lsp.lua
@@ -5,59 +5,33 @@ return {
 		"neovim/nvim-lspconfig",
 
 		-- Dependencies for the plugin
+		-- We are doing their configs in their own entry
 		dependencies = {
 			-- Signature hint plugin
 			"ray-x/lsp_signature.nvim",
 			-- Completion engine
 			"saghen/blink.cmp",
 		},
-		config = function()
-			-- Mostly copied from https://github.com/neovim/nvim-lspconfig#keybindings-and-completion
 
-			-- Use our custom function to set keymaps
+		config = function()
+			-- Keymaps for the lsp
 			local setmap = require("myconf/aux").setmap
 			local setmap_group_name = require("myconf/aux").setmap_group_name
-
-			-- Use an on_attach function to only map the following keys
-			-- after the language server attaches to the current buffer
-			local on_attach = function(client, bufnr)
-				local function buf_set_keymap(...)
-					vim.api.nvim_buf_set_keymap(bufnr, ...)
-				end
-				local function buf_set_option(...)
-					vim.api.nvim_buf_set_option(bufnr, ...)
-				end
-
-				-- Options for the mappings that work with buffers
-				local bufopts = { noremap = true, silent = true, buffer = bufnr }
-
-				-- Keymappings
-				setmap_group_name("<leader>q", "LSP commands")
-				setmap("n", "<leader>qr", vim.lsp.buf.rename, bufopts, "Rename")
-				setmap("n", "<leader>qd", vim.lsp.buf.definition, bufopts, "Go to definition")
-				setmap("n", "<leader>qi", vim.lsp.buf.implementation, bufopts, "Go to implementation")
-				setmap("n", "<leader>qa", vim.lsp.buf.code_action, bufopts, "Code action")
-				setmap("n", "<leader>ql", vim.lsp.buf.references, bufopts, "List references")
-				setmap("n", "<leader>qs", vim.lsp.buf.signature_help, bufopts, "Signature help")
-				setmap("n", "<leader>qh", vim.lsp.buf.hover, bufopts, "Hover (docs)")
-				setmap("n", "<leader>qf", function()
-					vim.lsp.buf.format({ async = true })
-				end, bufopts, "Manual Format")
-
-				-- Signature hint plugin
-				require("lsp_signature").on_attach({
-					bind = true, -- Mandatory for registring border config
-					hint_prefix = "🐼 ",
-					handler_opts = {
-						border = "rounded",
-					},
-				})
-			end
-
-			-- Keymaps that I want to have even when there is no running LSP
 			local opts = { noremap = true, silent = true }
+
+			setmap_group_name("<leader>q", "LSP commands")
 			setmap("n", "<leader>qe", vim.diagnostic.open_float, opts, "Open float")
 			setmap("n", "<leader>qc", vim.diagnostic.setloclist, opts, "Set location list")
+			setmap("n", "<leader>qr", vim.lsp.buf.rename, bufopts, "Rename")
+			setmap("n", "<leader>qd", vim.lsp.buf.definition, bufopts, "Go to definition")
+			setmap("n", "<leader>qi", vim.lsp.buf.implementation, bufopts, "Go to implementation")
+			setmap("n", "<leader>qa", vim.lsp.buf.code_action, bufopts, "Code action")
+			setmap("n", "<leader>ql", vim.lsp.buf.references, bufopts, "List references")
+			setmap("n", "<leader>qs", vim.lsp.buf.signature_help, bufopts, "Signature help")
+			setmap("n", "<leader>qh", vim.lsp.buf.hover, bufopts, "Hover (docs)")
+			setmap("n", "<leader>qf", function()
+				vim.lsp.buf.format({ async = true })
+			end, bufopts, "Manual Format")
 
 			-- Rounded borders for hover and signature help
 			vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
@@ -68,9 +42,7 @@ return {
 			vim.diagnostic.config({ virtual_text = true })
 			vim.diagnostic.config({ virtual_lines = { current_line = true } })
 
-			-- We need to manually specify which servers we want to configure
-			-- Some languages are commented, because I am not using them now but might use them in the future
-			-- As we have set `automatic_installation = true`, this servers are installed automatically
+			-- List of servers that I want to have access to
 			local servers = {
 
 				-- For markdown
@@ -95,9 +67,9 @@ return {
 				"nixd",
 
 				-- For python
-				"pylsp", -- General purpose lsp
-				"ruff", -- Linter and formatter
 				"ty", -- Static checking
+				"ruff", -- Linter and formatter
+				"pylsp", -- General purpose lsp
 
 				-- For writing some Ocaml
 				"ocamllsp",
@@ -105,31 +77,8 @@ return {
 				-- For writing typst documents
 				"tinymist",
 			}
-
-			-- This server is experimental and has to be manually enabled
-			vim.lsp.enable("ty")
-
-			-- Capabilities that blink completion engine needs to inform about
-			local capabilities = require("blink.cmp").get_lsp_capabilities()
-
-			-- Iterate over all installed servers and apply the config to them
-			for _, server in ipairs(servers) do
-				-- Setup given server
-				require("lspconfig")[server].setup({
-
-					on_attach = on_attach,
-
-					-- Capabilities that were modified to talk with nvim-cmp
-					capabilities = capabilities,
-
-					-- Options to the lsp configuration
-					settings = {
-						-- For better performance
-						flags = {
-							debounce_text_changes = 150,
-						},
-					},
-				})
+			for _, server in pairs(servers) do
+				vim.lsp.enable(server)
 			end
 		end,
 	},
@@ -139,5 +88,18 @@ return {
 		"j-hui/fidget.nvim",
 		tag = "legacy",
 		config = true,
+	},
+
+	-- Signature plugin
+	{
+		"ray-x/lsp_signature.nvim",
+		config = {
+
+			bind = true, -- Mandatory for registring border config
+			hint_prefix = "🐼 ",
+			handler_opts = {
+				border = "rounded",
+			},
+		},
 	},
 }
